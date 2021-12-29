e139dd25e283cfc9090c1a5487eb9b2385a4612b
Sil3ntStorm <Sil3ntStorm@users.noreply.github.com>
2021-12-29 03:55:17 +0100

    Initial version

---
+++ control.lua
@@ -0,0 +1,54 @@
+-- Copyright 2021 Sil3ntStorm https://github.com/Sil3ntStorm
+--
+-- Licensed under MS-RL, see https://opensource.org/licenses/MS-RL
+
+local function getDistance(pos, tgt)
+    local x = (tgt.x - pos.x) ^ 2
+    local y = (tgt.y - pos.y) ^ 2
+    local d = (x + y) ^ 0.5
+    return d
+end
+
+local function getClosestTileName(tiles, pos)
+    local result = 'dry-dirt'
+    local lowest = 30
+    for _, tile in pairs(tiles) do
+        if tile.name ~= 'water' and tile.name ~= 'deepwater' then
+            local dist = getDistance(tile.position, pos)
+            if tile.name ~= 'nuclear-ground' and dist < lowest then
+                lowest = dist
+                result = tile.name
+            end
+        end
+    end
+    return result
+end
+
+local function onSelection(data)
+    local hasMower = script.active_mods['Mower'] ~= nil
+    if hasMower and data.item ~= 'mower-mower' then
+        return
+    elseif not hasMower and data.item ~= 'rmnmks-remove-tool' then
+        return
+    end
+    local plr = game.get_player(data.player_index)
+    if (not hasMower and not plr.force.technologies['atomic-bomb'].researched) then
+        return
+    end
+    data.surface.destroy_decoratives{area = data.area, name = 'nuclear-ground-patch'}
+    local found = data.surface.find_tiles_filtered{area = data.area, name = 'nuclear-ground'}
+    if #found > 0 then
+        local tiles = data.surface.find_tiles_filtered{area = data.area}
+        local nTiles = {}
+        for _, tile in pairs(found) do
+            table.insert(nTiles, {name = getClosestTileName(tiles, tile.position), position = tile.position})
+        end
+        data.surface.set_tiles(nTiles)
+    end
+    found = data.surface.find_entities_filtered{area = data.area, name = {'nuclear-smouldering-smoke-source'}}
+    for _, ent in pairs(found) do
+        ent.destroy()
+    end
+end
+
+script.on_event({defines.events.on_player_selected_area, defines.events.on_player_alt_selected_area}, onSelection)
---
+++ data.lua
@@ -0,0 +1,65 @@
+if not mods['Mower'] then
+    data:extend{
+        {
+            type = 'custom-input',
+            name = 'rmnmks-remove',
+            key_sequence = 'CONTROL + ALT + X',
+            action = 'spawn-item',
+            item_to_spawn = 'rmnmks-remove-tool',
+            technology_to_unlock = 'atomic-bomb',
+        },
+        {
+            type = 'selection-tool',
+            name = 'rmnmks-remove-tool',
+            icon = '__remove-nuclear-marks__/graphics/icons/icon-crossed.png',
+            icon_size = 32,
+            flags = {'only-in-cursor', 'hidden', 'spawnable'},
+            stack_size = 1,
+            stackable = false,
+            toggleable = false,
+            show_in_library = false,
+            subgroup = 'tool',
+            selection_mode = 'nothing',
+            selection_color = { r = 0.7, b = 0.7 },
+            selection_cursor_box_type = 'not-allowed',
+            alt_selection_mode = 'nothing',
+            alt_selection_color = { r = 0.7, b = 0.7 },
+            alt_selection_cursor_box_type = 'not-allowed'
+        },
+        {
+            type = 'shortcut',
+            name = 'rmnmks-remove-shortcut',
+            associated_control_input = 'rmnmks-remove',
+            action = 'spawn-item',
+            item_to_spawn = 'rmnmks-remove-tool',
+            technology_to_unlock = 'atomic-bomb',
+            icon =
+            {
+                filename = '__remove-nuclear-marks__/graphics/icons/icon-crossed.png',
+                priority = 'extra-high-no-scale',
+                size = 32,
+                scale = 0.5,
+                mipmap_count = 2,
+                flags = {'gui-icon'}
+            },
+            small_icon =
+            {
+                filename = '__remove-nuclear-marks__/graphics/icons/icon-crossed.png',
+                priority = 'extra-high-no-scale',
+                size = 32,
+                scale = 0.5,
+                mipmap_count = 2,
+                flags = {'gui-icon'}
+            },
+            disabled_small_icon =
+            {
+                filename = '__remove-nuclear-marks__/graphics/icons/icon-crossed.png',
+                priority = 'extra-high-no-scale',
+                size = 32,
+                scale = 0.5,
+                mipmap_count = 2,
+                flags = {'gui-icon'}
+            }
+        }
+    }
+end
